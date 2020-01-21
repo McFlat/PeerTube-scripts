bde341269c8ab3bba394fe158201f5d959c1a25f
BRAINS YUM <43896676+McFlat@users.noreply.github.com>
2020-01-21 08:40:35 -0600

    Create find-name.sh

---
+++ find-name.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# return two results from peertube site search api; used for checking if duplicate videos have been imported into the site
+
+if [ "$(which wget)" == "" ]; then
+  apt install wget;
+fi
+if [ "$(which jq)" == "" ]; then
+  apt install jq;
+fi
+
+QUERY="$@";
+PEERTUBE_URL="https://troo.tube";
+data=$(wget -q "${PEERTUBE_URL}/api/v1/search/videos?start=0&count=2&sort=-match&search=${QUERY}" -O -);
+one=$(echo $data | jq -r .data[0].uuid,.data[0].name);
+two=$(echo $data | jq -r .data[1].uuid,.data[1].name);
+if [ "$one" == "null" ]; then echo ""; else echo -e $one; fi;
+if [ "$two" == "null" ]; then echo ""; else echo -e $two; fi;
