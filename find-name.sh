6b88cd8abf1a00fbb7855e55e593ab72964ff931
BRAINS YUM <43896676+McFlat@users.noreply.github.com>
2020-01-21 12:34:38 -0600

    Update find-name.sh

--- find-name.sh
+++ find-name.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# return two results from peertube site search api; used for checking if duplicate videos have been imported into the site
+# return two results from peertube site search api, used for checking if duplicate videos have been imported into the site
 
 if [ "$(which wget)" == "" ]; then
   apt install wget;
@@ -12,7 +12,7 @@ fi
 QUERY="$@";
 PEERTUBE_URL="https://troo.tube";
 data=$(wget -q "${PEERTUBE_URL}/api/v1/search/videos?start=0&count=2&sort=-match&search=${QUERY}" -O -);
-one=$(echo $data | jq -r .data[0].uuid,.data[0].name);
-two=$(echo $data | jq -r .data[1].uuid,.data[1].name);
-if [ "$one" == "null" ]; then echo ""; else echo -e $one; fi;
-if [ "$two" == "null" ]; then echo ""; else echo -e $two; fi;
+one=$(echo $data | jq -r .data[0].id,.data[0].uuid,.data[0].name);
+two=$(echo $data | jq -r .data[1].id,.data[1].uuid,.data[1].name);
+if [ "$(echo $one | awk '{print $1}')" == "null" ]; then echo ""; else echo -e $one; fi;
+if [ "$(echo $two | awk '{print $1}')" == "null" ]; then echo ""; else echo -e $two; fi;
